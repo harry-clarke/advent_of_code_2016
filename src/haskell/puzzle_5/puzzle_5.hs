f8b2153e7c36b0df71e248059f15983e9bc4c027
Harry Clarke <harry-clarke@outlook.com>
2017-09-19 15:14:03 +0100

    Completed puzzle 5, part 2.

--- src/haskell/puzzle_5/puzzle_5.hs
+++ src/haskell/puzzle_5/puzzle_5.hs
@@ -2,6 +2,7 @@ module Puzzle_5 where
 
 import Crypto.Hash.MD5
 import Data.List
+import Data.Maybe
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
@@ -19,7 +20,22 @@ matchingHashes s = map (drop 5) $ filter ("00000" `isPrefixOf`) [hashAndHex m | 
 decrypt_1 :: String -> String
 decrypt_1 = foldr ((:) . (!! 0)) "" . take 8 . matchingHashes
 
+decrypt_2 :: String -> String
+decrypt_2 = decrypt_2' (replicate 8 Nothing) . filter ((`elem` ['0'..'7']) . head) . matchingHashes
+  where
+    decrypt_2' :: [Maybe Char] -> [String] -> String
+    decrypt_2' d hs
+      | all isJust d = map fromJust d
+      | ((i:d':_):hs) <- hs = decrypt_2' (swap d (read [i]) d') hs
+    swap :: [Maybe Char] -> Int -> Char -> [Maybe Char]
+    swap d i c =
+      case d !! i of
+        Just _ -> d
+        Nothing -> let (x,(y:ys)) = splitAt i d in x ++ (Just c):ys
+
 main :: IO ()
 main = do
   let a1 = decrypt_1 input
-  print a1
+  let a2 = decrypt_2 input
+  --print a1
+  print a2
