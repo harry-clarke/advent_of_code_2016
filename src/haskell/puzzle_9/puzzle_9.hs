f163609758c9d4dcea70cc18c0426bc949bb163a
Harry Clarke <harry-clarke@outlook.com>
2017-09-20 21:22:17 +0100

    Completed puzzle 9, part 2.

--- src/haskell/puzzle_9/puzzle_9.hs
+++ src/haskell/puzzle_9/puzzle_9.hs
@@ -13,6 +13,11 @@ data Instr = Instr Span Count DataSec Rest
 break' :: (a -> Bool) -> [a] -> ([a] , [a])
 break' f l = let (a , b) = break f l in (a , tail b)
 
+splitAt' :: Int -> [a] -> ([a], [a])
+splitAt' i l
+  | length l >= i = splitAt i l
+  | otherwise = error "Instruction overuns."
+
 parseInstruction :: String -> Maybe Instr
 parseInstruction ('(':rest) =
   Just (Instr span count dataSec rest')
@@ -21,29 +26,30 @@ parseInstruction ('(':rest) =
     b' = break' (==')') $ snd b
     span = read $ fst b
     count = read $ fst b'
-    (dataSec , rest') = splitAt span $ snd b'
+    (dataSec , rest') = splitAt' span $ snd b'
 parseInstruction _ = Nothing
 
-convert :: String -> String
-convert "" = ""
-convert s
+answer_1 :: String -> String
+answer_1 "" = ""
+answer_1 s
   | Just (Instr span count sec rest) <- parseInstruction s =
     let pre = take (length sec * count) $ cycle sec in
-    (pre ++ convert rest)
-  | (c:cs) <- s = (c : convert cs)
+    (pre ++ answer_1 rest)
+  | (c:cs) <- s = (c : answer_1 cs)
 
-{-
-count :: String -> Int
-count "" = 0
-count s
-  | Just ((span,count), rest) <- parseInstruction s =
-    let (sec , rest') = splitAt span rest in
--}
+answer_2 :: String -> Int
+answer_2 "" = 0
+answer_2 s
+  | Just (Instr span c sec rest) <- parseInstruction s =
+    c * answer_2 sec + answer_2 rest
+  | otherwise = 1 + answer_2 (tail s)
 
 main :: IO ()
 main = do
   handle <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
   let i = filter (not . isSpace) contents
-  let a = length $ convert i
-  print a
+  let a1 = length $ answer_1 i
+  let a2 = answer_2 i
+  print a1
+  print a2
