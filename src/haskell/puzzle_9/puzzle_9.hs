fddc3cb00eeb19f664b4818e722e9125ae8550ba
Harry Clarke <harry-clarke@outlook.com>
2017-09-20 21:08:04 +0100

    Refactored puzzle 9.

--- src/haskell/puzzle_9/puzzle_9.hs
+++ src/haskell/puzzle_9/puzzle_9.hs
@@ -6,26 +6,39 @@ import Data.Char
 
 type Span = Int
 type Count = Int
-type Instr = (Span , Count)
+type DataSec = String
+type Rest = String
+data Instr = Instr Span Count DataSec Rest
 
 break' :: (a -> Bool) -> [a] -> ([a] , [a])
 break' f l = let (a , b) = break f l in (a , tail b)
 
-parseInstruction :: String -> Maybe (Instr , String)
+parseInstruction :: String -> Maybe Instr
 parseInstruction ('(':rest) =
-  let (span , rest') = break' (=='x') rest in
-  let (count , rest'') = break' (==')') rest' in
-  Just ((read span , read count) , rest'')
+  Just (Instr span count dataSec rest')
+  where
+    b = break' (=='x') rest
+    b' = break' (==')') $ snd b
+    span = read $ fst b
+    count = read $ fst b'
+    (dataSec , rest') = splitAt span $ snd b'
 parseInstruction _ = Nothing
 
 convert :: String -> String
 convert "" = ""
 convert s
+  | Just (Instr span count sec rest) <- parseInstruction s =
+    let pre = take (length sec * count) $ cycle sec in
+    (pre ++ convert rest)
+  | (c:cs) <- s = (c : convert cs)
+
+{-
+count :: String -> Int
+count "" = 0
+count s
   | Just ((span,count), rest) <- parseInstruction s =
     let (sec , rest') = splitAt span rest in
-    let pre = take (length sec * count) $ cycle sec in
-    (pre ++ convert rest')
-  | (c:cs) <- s = (c : convert cs)
+-}
 
 main :: IO ()
 main = do
